401bfd11149bfe7d971bd322171578f71fba9555
Niktia <34900194+Niktia@users.noreply.github.com>
2017-12-27 18:35:46 -0600

    Add files via upload

---
+++ MyApplication.java
@@ -0,0 +1,14 @@
+
+public class MyApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My name is H Nikita Lovett."
+				+ " My Csu Id is 900721043.");
+		
+		System.out.println("I am learning Advanced Programming in Java.");
+
+	}
+
+}
